992fd34ec3c97f37ec5083b31b6b48b79851cefc
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-11-26 22:58:12 +0700

    feat: Added insert restanrant repository

---
+++ repository/restaurant.go
@@ -0,0 +1,32 @@
+package repository
+
+import (
+	model "github.com/BounkBU/kurester/models"
+	"github.com/jmoiron/sqlx"
+)
+
+type restaurantRepository struct {
+	db *sqlx.DB
+}
+
+type RestaurantRepository interface {
+	InsertRestarant(restaurant model.Restaurant) error
+}
+
+func NewRestaurantRepository(db *sqlx.DB) *restaurantRepository {
+	return &restaurantRepository{
+		db: db,
+	}
+}
+
+func (r *restaurantRepository) InsertRestarant(restaurant model.Restaurant) error {
+	_, err := r.db.Query(`
+	INSERT INTO restaurant (name, latitude, longitude)
+	VALUES (?, ?, ?)
+`,
+		restaurant.Name,
+		restaurant.Latitude,
+		restaurant.Longitude,
+	)
+	return err
+}
