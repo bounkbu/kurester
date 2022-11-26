051a48ab14d75913cfce6d5a8d326768706d63ee
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-11-27 01:59:25 +0700

    fix: Added tables to database

--- pkg/database/migration/000001_init_schema.down.sql
+++ pkg/database/migration/000001_init_schema.down.sql
@@ -1,2 +1,5 @@
 DROP TABLE IF EXISTS `restaurant`;
 DROP TABLE IF EXISTS `menu`;
+DROP TABLE IF EXISTS `faculty`;
+DROP TABLE IF EXISTS `restaurant_popularity`;
+DROP TABLE IF EXISTS `submit_form`;
--- pkg/database/migration/000001_init_schema.up.sql
+++ pkg/database/migration/000001_init_schema.up.sql
@@ -16,13 +16,36 @@ CREATE TABLE `menu` (
   `created_at` TIMESTAMP NOT NULL DEFAULT (now())
 );
 
-ALTER TABLE `menu` ADD FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant` (`id`);
+CREATE TABLE `faculty` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255),
+  `latitude` decimal(20, 14),
+  `longitude` decimal(20, 14),
+  `created_at` TIMESTAMP NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE `restaurant_popularity` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `restaurant_id` INT,
+  `popularity` INT
+);
+
+CREATE TABLE `submit_form` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `faculty_id` INT,
+  `type` varchar(255),
+  `price` FLOAT,
+  `is_spicy` BOOLEAN
+);
 
 INSERT INTO `restaurant` (name, latitude, longitude)
 VALUES ('Starbucks (Faculty of Science KU)', 13.845874718049188, 100.57244344441678);
 
 INSERT INTO `restaurant` (name, latitude, longitude)
 VALUES ('Cafe Dot Com', 13.846039538925096, 100.56871499739482);
+
+INSERT INTO `restaurant` (name, latitude, longitude)
+VALUES ('TokiDoki Curry Kaset-Phaholypthin', 13.840252544964363, 100.57370500451941);
 
 INSERT INTO `menu` (restaurant_id, name, type, price, is_spicy)
 VALUES (1, 'Americano', 'drink', 120, false);
@@ -31,4 +54,7 @@ VALUES (1, 'Latte', 'drink', 140, false);
 
 INSERT INTO `menu` (restaurant_id, name, type, price, is_spicy)
 VALUES (2, 'Americano', 'drink', 40, false);
+
+INSERT INTO `menu` (restaurant_id, name, type, price, is_spicy)
+VALUES (3, 'Katsu Curry', 'rice', 120, false);
 
