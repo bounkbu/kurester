0c65a34d16f7c39a6f95f07f578ae9ef087ab251
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-11-26 22:07:39 +0700

    feat: Added new connection url builder

---
+++ pkg/util/connection_url_builder.go
@@ -0,0 +1,35 @@
+package util
+
+import (
+	"fmt"
+
+	"github.com/BounkBU/kurester/config"
+	log "github.com/sirupsen/logrus"
+)
+
+func NewConnectionUrlBuilder(stuff string, db config.Database) string {
+	var url string
+	switch stuff {
+	case "mysql":
+		url = fmt.Sprintf(
+			"%s:%s@tcp(%s:%s)/%s?parseTime=true",
+			db.Username,
+			db.Password,
+			db.Hostname,
+			db.Port,
+			db.Database,
+		)
+	case "dns":
+		url = fmt.Sprintf(
+			"%s:%s@tcp(%s)/%s?tls=true",
+			db.Username,
+			db.Password,
+			db.Hostname,
+			db.Database,
+		)
+	default:
+		log.Errorf("error, unknow the stuff: %s", stuff)
+	}
+
+	return url
+}
