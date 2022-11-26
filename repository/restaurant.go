c6152d3616dd8e8a452898d5bcb32c284ccd7514
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-11-27 02:03:16 +0700

    fix: Removed unused function

--- repository/restaurant.go
+++ repository/restaurant.go
@@ -11,7 +11,6 @@ type restaurantRepository struct {
 
 type RestaurantRepository interface {
 	InsertRestarant(model.Restaurant) error
-	SelectNearbyRestaurant(model.User) ([]model.Restaurant, error)
 }
 
 func NewRestaurantRepository(db *sqlx.DB) *restaurantRepository {
@@ -30,19 +29,4 @@ func (r *restaurantRepository) InsertRestarant(restaurant model.Restaurant) erro
 		restaurant.Longitude,
 	)
 	return err
-}
-
-func (s *restaurantRepository) SelectNearbyRestaurant(user model.User) ([]model.Restaurant, error) {
-	var restaurants []model.Restaurant
-	err := s.db.Select(&restaurants, `
-		SELECT name, latitude, longitude, SQRT(
-			POW(69.1 * (latitude - %f), 2) +
-			POW(69.1 * (%f - longitude) * COS(latitude / 57.3), 2)) AS distance
-		FROM restaurant HAVING distance < 5 ORDER BY distance LIMIT 10;
-	`, user.Latitude, user.Longitude)
-	if err != nil {
-		return restaurants, err
-	}
-
-	return restaurants, nil
 }
