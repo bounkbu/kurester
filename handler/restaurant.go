d4b06863c7a360538e3d7d15ca486e13cf698203
Pakapop Cheunchomsirakul <abounk@Pakapops-MacBook-Pro.local>
2022-11-27 04:23:10 +0700

    Add API for getting popular resturant

--- handler/restaurant.go
+++ handler/restaurant.go
@@ -1,11 +1,14 @@
 package handler
 
 import (
+	"database/sql"
+	"errors"
 	"net/http"
 
 	model "github.com/BounkBU/kurester/models"
 	"github.com/BounkBU/kurester/service"
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 )
 
 type restaurantHandler struct {
@@ -34,4 +37,23 @@ func (h *restaurantHandler) CreateNewRestaurantHandler(c *gin.Context) {
 
 	message := "create new restaurant successfully"
 	c.JSON(http.StatusOK, messageResponse(message))
+}
+
+func (h *restaurantHandler) GetPopularRestaurant(c *gin.Context) {
+	out, err := h.restaurantService.GetPopularRestaurant()
+	if err == nil {
+		c.JSON(http.StatusOK, out)
+		log.Info("return popular resturants")
+		return
+	} else if errors.Is(err, sql.ErrNoRows) {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "Not matching data",
+		})
+		log.Error(err)
+		return
+	}
+	c.JSON(http.StatusInternalServerError, gin.H{
+		"message": "Something went wrong.",
+	})
+	log.Error(err)
 }
--- service/restaurant.go
+++ service/restaurant.go
@@ -12,6 +12,7 @@ type restaurantService struct {
 
 type RestaurantService interface {
 	CreateNewRestaurant(model.Restaurant) error
+	GetPopularRestaurant() ([]model.Restaurant, error)
 }
 
 func NewRestaurantService(restaurantRepository repository.RestaurantRepository) *restaurantService {
@@ -26,4 +27,15 @@ func (s *restaurantService) CreateNewRestaurant(newRestaurant model.Restaurant) 
 
 	err := s.restaurantRepository.InsertRestarant(newRestaurant)
 	return err
+}
+
+func (s *restaurantService) GetPopularRestaurant() (restaurants []model.Restaurant, err error) {
+	log.Info("Start getting popular restaurant")
+	defer log.Info("End getting popular restaurant")
+
+	restaurants, err = s.restaurantRepository.QueryPopularRestaurant()
+	if err != nil {
+		log.Error(err)
+	}
+	return
 }
