26ca9a937b920b7b45a1869610dad32eea9b9e47
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-11-27 01:59:09 +0700

    fix: Allowed cors for origin

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/BounkBU/kurester
 go 1.19
 
 require (
+	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.8.1
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/jmoiron/sqlx v1.3.5
--- httpserver/httpserver.go
+++ httpserver/httpserver.go
@@ -5,6 +5,7 @@ import (
 	"github.com/BounkBU/kurester/handler"
 	"github.com/BounkBU/kurester/repository"
 	"github.com/BounkBU/kurester/service"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
 	log "github.com/sirupsen/logrus"
@@ -27,16 +28,20 @@ func NewHTTPServer(config *config.Config, db *sqlx.DB) *Server {
 }
 
 func (server *Server) SetUpRouter() {
+	server.App.Use(cors.Default())
 	server.App.GET("/", handler.HealthCheckHandler)
 
 	restaurantRepository := repository.NewRestaurantRepository(server.Database)
-	restaurantService := service.NewRestaurantService(restaurantRepository)
-	restaurantHandler := handler.NewRestaurantHandler(restaurantService)
-	server.App.POST("/restarants", restaurantHandler.CreateNewRestaurantHandler)
-
 	menuRepository := repository.NewMenuRepository(server.Database)
+
+	restaurantService := service.NewRestaurantService(restaurantRepository)
 	menuService := service.NewMenuService(menuRepository)
+	// userService := service.NewUserService(restaurantRepository)
+
+	restaurantHandler := handler.NewRestaurantHandler(restaurantService)
 	menuHandler := handler.NewMenuHandler(menuService)
+
+	server.App.POST("/restarants", restaurantHandler.CreateNewRestaurantHandler)
 	server.App.POST("/menus", menuHandler.CreateNewMenuHandler)
 }
 
