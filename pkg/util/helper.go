43712257182232bbddef4877e156c342d6e8646a
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-11-28 06:13:03 +0700

    fix: price count helper function

--- pkg/util/helper.go
+++ pkg/util/helper.go
@@ -1,14 +1,14 @@
 package util
 
 func PriceCountingHelper(price float64) string {
-	if price >= 200 {
-		return "200++"
-	} else if price >= 151 {
-		return "151-200"
-	} else if price >= 101 {
-		return "101-150"
-	} else if price >= 51 {
+	if price <= 50 {
+		return "0-50"
+	} else if price <= 100 {
 		return "51-100"
+	} else if price <= 150 {
+		return "101-150"
+	} else if price <= 200 {
+		return "151-200"
 	}
-	return "0-50"
+	return "200++"
 }
