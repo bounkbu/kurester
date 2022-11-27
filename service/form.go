6f015f43c49cfe91ccf2e2f5f068bd84f7b143a7
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-11-28 01:06:37 +0700

    feat: Added create new form  service

---
+++ service/form.go
@@ -0,0 +1,29 @@
+package service
+
+import (
+	model "github.com/BounkBU/kurester/models"
+	"github.com/BounkBU/kurester/repository"
+	log "github.com/sirupsen/logrus"
+)
+
+type formService struct {
+	formRepository repository.FormRepository
+}
+
+type FormService interface {
+	CreateNewForm(model.Form) error
+}
+
+func NewFormService(formRepository repository.FormRepository) *formService {
+	return &formService{
+		formRepository: formRepository,
+	}
+}
+
+func (s *formService) CreateNewForm(newForm model.Form) error {
+	log.Info("Start creating new form")
+	defer log.Info("End creating new form")
+
+	err := s.formRepository.InsertForm(newForm)
+	return err
+}
