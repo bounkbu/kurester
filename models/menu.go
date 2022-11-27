7d6ff241bf455e046bb2b6ca8e0cfd7e1453f3c2
Thanathip <dannajaeiei@hotmail.com>
2022-11-28 03:55:56 +0700

    feat: Create recommeded menu model

--- models/menu.go
+++ models/menu.go
@@ -12,3 +12,14 @@ type Menu struct {
 	IsSpicy      bool      `json:"is_spicy" db:"is_spicy"`
 	CreatedAt    time.Time `json:"created_at" db:"created_at"`
 }
+
+type RecommendedMenu struct {
+	Id         int64      `json:"id" db:"id"`
+	Restaurant Restaurant `json:"restaurant"`
+	Name       string     `json:"name" db:"name"`
+	PictureUrl string     `json:"picture_url" db:"pictureUrl"`
+	Type       string     `json:"type" db:"type"`
+	Price      float64    `json:"price" db:"price"`
+	IsSpicy    bool       `json:"is_spicy" db:"is_spicy"`
+	CreatedAt  time.Time  `json:"created_at" db:"created_at"`
+}
