2696a9d4ca04adc98e069bcb57a081f74f33640b
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-11-28 22:30:30 +0700

    feat: Added query for popularity ratio

--- repository/ratio.go
+++ repository/ratio.go
@@ -13,6 +13,8 @@ type RatioRepository interface {
 	QueryIsSpicyRatio() ([]model.SpicynessRatio, error)
 	QuerySubmitFormPrice() ([]model.SubmitFormPrice, error)
 	QueryFoodTypeRatio() ([]model.FoodTypeRatio, error)
+	QueryPopularityFromAverageMenuPrice() ([]model.PopularityFromAverageMenuPrice, error)
+	QueryAveragePopularityFromPrice() ([]model.AveragePopularityFromPrice, error)
 }
 
 func NewRatioRepository(db *sqlx.DB) *ratioRepository {
@@ -69,4 +71,50 @@ func (r *ratioRepository) QueryFoodTypeRatio() ([]model.FoodTypeRatio, error) {
 
 	logger.Info("Query food type ratio")
 	return foodTypeRatio, nil
+}
+
+func (r *ratioRepository) QueryPopularityFromAverageMenuPrice() ([]model.PopularityFromAverageMenuPrice, error) {
+	logger := generateLogger("QueryPopularityFromAverageMenuPrice")
+
+	var popularity []model.PopularityFromAverageMenuPrice
+	err := r.db.Select(&popularity, `
+		SELECT (SELECT name FROM restaurant WHERE id=restaurant_id) as restaurant_name, AVG(price) as average_price, MIN(popularity) popularity
+		FROM (
+		SELECT m.restaurant_id, price, popularity
+		FROM menu m
+		INNER JOIN restaurant_popularity rp
+		ON m.restaurant_id = rp.restaurant_id
+		) avg_price
+		GROUP BY restaurant_id;
+	`)
+	if err != nil {
+		logger.Error(err)
+		return nil, err
+	}
+
+	logger.Info("Query popularity from average menu price")
+	return popularity, nil
+}
+
+func (r *ratioRepository) QueryAveragePopularityFromPrice() ([]model.AveragePopularityFromPrice, error) {
+	logger := generateLogger("QueryPopularityFromAverageMenuPrice")
+
+	var averagePopularity []model.AveragePopularityFromPrice
+	err := r.db.Select(&averagePopularity, `
+		SELECT price, AVG(popularity) average_popularity
+		FROM (
+		SELECT m.restaurant_id, price, popularity
+		FROM menu m
+		INNER JOIN restaurant_popularity rp
+		ON m.restaurant_id = rp.restaurant_id
+		) avg_price
+		GROUP BY price;
+	`)
+	if err != nil {
+		logger.Error(err)
+		return nil, err
+	}
+
+	logger.Info("Query average popularity from price")
+	return averagePopularity, nil
 }
