0601d8de929a853e5eb5729222ab90a2ebfb3dac
Thanathip <dannajaeiei@hotmail.com>
2022-11-29 02:26:44 +0700

    docs: Gererate docs and change host, schemes

--- docs/docs.go
+++ docs/docs.go
@@ -502,9 +502,9 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8888",
+	Host:             "kurester.herokuapp.com",
 	BasePath:         "",
-	Schemes:          []string{"http"},
+	Schemes:          []string{"https"},
 	Title:            "KU Rester API",
 	Description:      "The KU Rester web API",
 	InfoInstanceName: "swagger",
